93d906c94e55d43d8c3353eb8e8cf368238f61b5
barry <barry@qq.com>
2022-04-01 10:18:15 +0800

    second commit

--- src/main/java/suzhou/com/GitTest.java
+++ src/main/java/suzhou/com/GitTest.java
@@ -3,5 +3,6 @@ package suzhou.com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
